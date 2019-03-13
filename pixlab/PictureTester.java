ca6d7ffb9628a1b9a52b8243f2c2e3e0a313e966
Rachelsong619 <31286711+Rachelsong619@users.noreply.github.com>
2019-03-13 11:07:40 -0700

    Update PictureTester.java

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -41,6 +41,14 @@ public class PictureTester
       caterpillar.mirrorHorizontal();
       caterpillar.explore();
   }
+  
+  public static void testMirrorDiagnoal()
+  {
+    Picture caterpillar = new Picture ("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorDiagnoal();
+    caterpillar.explore();
+  }
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
